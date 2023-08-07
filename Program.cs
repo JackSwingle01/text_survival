238d12f3a7987def6d862e4f7ef892529761e485
jackswingle01 <jack774411@gmail.com>
2023-08-07 12:07:57 -0400

    moved game logic to program

--- Program.cs
+++ Program.cs
@@ -1,11 +1,26 @@
-﻿namespace text_survival
+﻿using text_survival.Environments;
+
+namespace text_survival
 {
     public class Program
     {
         static void Main()
         {
-            Game game = new();
-            game.Start();
+            Utils.WriteLine("You have been banished from your home city.");
+            Thread.Sleep(1000);
+            Utils.WriteLine("Stripped of your possessions, you've been left to fend for yourself in the unforgiving wilderness.");
+            Thread.Sleep(1000);
+            Utils.WriteLine("The ancient laws, however, grant one path to redemption.");
+            Thread.Sleep(1000);
+            Utils.WriteLine("You must defeat a Dragon...\n");
+            Thread.Sleep(2000);
+            Player player = new Player(AreaFactory.GenerateArea(Area.EnvironmentType.Forest));
+            World.Time = new TimeOnly(hour: 9, minute: 0);
+            Actions actions = new(player);
+            while (player.Health > 0)
+            {
+                actions.Act();
+            }
         }
     }
 }
