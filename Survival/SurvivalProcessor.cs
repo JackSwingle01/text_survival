45443bae4a02b2dfa193e2404f0033d4a5adbdc8
Jack Swingle <83100248+JackSwingle01@users.noreply.github.com>
2025-06-30 14:15:11 -0400

    Create SurvivalProcessor.cs

---
+++ Survival/SurvivalProcessor.cs
@@ -0,0 +1,17 @@
+namespace text_survival.Survival;
+public class SurvivalData
+{
+  public double Hunger;
+  public double Thirst;
+  public double Exhaustion;
+  public double Temperature;
+}
+public static class SurvivalProcessor
+{
+  public static SurvivalData Process(SurvivalData data)
+  {
+    
+     return data; 
+  }
+  
+}
