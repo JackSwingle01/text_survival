6778123a3b5c2f7e453c922a5539ae8457f7e2e4
Jack Swingle <83100248+JackSwingle01@users.noreply.github.com>
2025-06-30 14:27:16 -0400

    Add rest to survival processor

--- Survival/SurvivalProcessor.cs
+++ Survival/SurvivalProcessor.cs
@@ -9,12 +9,19 @@ public class SurvivalData
 public static class SurvivalProcessor
 {
   private const double BASE_EXHAUSTION_RATE = 1;
-  private const double MAX_EXHAUSTION_MINUTES  = 480.0F; // minutes (8 hours)
+  private const double MAX_EXHAUSTION_MINUTES  = 960.0F; // minutes (16 hours)
   
   public static SurvivalData Process(SurvivalData data, int minutesElapsed=1)
   {
-    data.Exhaustion = Math.Min(1, data.Exhaustion + BASE_EXHAUSTION_RATE * minutesElapsed);
+    data.Exhaustion = Math.Min(1, data.Exhaustion + (BASE_EXHAUSTION_RATE * minutesElapsed));
     return data; 
+  }
+
+  public static SurvivalData Sleep(SurvivalData data, int minutes)
+  {
+    // rest restores exhaustion at 2x the rate that you gain it while awake, so 16 hours of wakefulness creates only 8 hours of sleep debt
+    data.Exhaustion = Math.Max(0, data.Exhaustion - (BASE_EXHAUSTION_RATE * 2 * minutesElapsed)); 
+    return data;
   }
   
 }
